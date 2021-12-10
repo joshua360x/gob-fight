266becab684f0d6bd0fcba7b8e24c4076fbe29c6
joshua360x <jkw5499@psu.edu>
2021-12-10 15:20:45 -0500

    general functionality

--- app.js
+++ app.js
@@ -1,8 +1,105 @@
 // import functions and grab DOM elements
+import { renderGoblin } from './utils.js';
+
+
+const goblinForm = document.querySelector('#new-goblin-form');
+const playerHP = document.querySelector('.playerHP');
+const playerChar = document.querySelector('.player-Char');
+const goblinChars = document.querySelector('.evil-Goblins');
+const goblinDefeated = document.querySelector('.numberDefeated');
 
 // let state
+// let goblin = {
+//     name: '',
+//     hp: 0
+// };
+
+let newGob = [
+    {
+        name: 'Curry',
+        hp: 10
+    },
+    {
+        name: 'pizza',
+        hp: 15
+    },
+    {
+        name: 'yoyo',
+        hp: 20
+    }
+];
+let defeated = 0;
+
+let defeatedGoblin = [
+    {
+        name: ''
+    }
+];
 
 // set event listeners 
   // get user input
+goblinForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let goblinNew = new FormData(goblinForm);
+    let name = goblinNew.get('goblin-name');
+    let obj = {
+        name,
+        hp: (Math.ceil(Math.random() * 10))
+    };
+
+    newGob.push(obj);
+    displayGodblins();
+});
   // use user input to update state 
+function displayGodblins() {
+    goblinChars.textContent = '';
+    
+    for (let goblin of newGob) {
+
+        let goblinList = renderGoblin(goblin);
+
+        goblinList.addEventListener('click', () => {
+            if ((Math.random() * 5) > 2) {
+                alert('you have hit the goblin');
+                goblin.hp = goblin.hp - (Math.ceil(Math.random() * 3));
+                displayGodblins(); 
+            } 
+
+            if (goblin.hp <= 0) {
+                goblin.hp = 0;
+                defeatedGoblin.push(goblin);
+                defeated++;
+                goblinDefeated.textContent = defeated;
+                alert(`you have defeated ${goblin.name}`);
+          
+                    // console.log("🚀 ~ file: app.js ~ line 66 ~ goblinList.addEventListener ~ defeatedGoblin", defeatedGoblin)
+          
+                displayGodblins(); 
+            }
+
+
+
+        //   if (goblin.hp > 0) {
+        //              alert('you have hit the goblin')
+        //              goblin.hp = goblin.hp - (Math.ceil(Math.random() * 3))
+        //              displayGodblins() 
+        //            }
+        // }
+            else if ((Math.random() * 5) < 2.5) {
+                alert('you have missed the goblin');
+
+
+          // console.log("🚀 ~ file: app.js ~ line 66 ~ goblinList.addEventListener ~ defeatedGoblin", defeatedGoblin)
+
+                displayGodblins(); 
+            }
+
+        
+        });
+
+        goblinChars.append(goblinList);
+
+    }
+}
   // update DOM to reflect the new state
+displayGodblins();
---
+++ utils.js
@@ -0,0 +1,14 @@
+export function renderGoblin(data) {
+    const div = document.createElement('div');
+    let pName = document.createElement('p');
+    let img = document.createElement('img');
+    img.src = './assets/dragons-head-310697.svg';
+    let pHP = document.createElement('p');
+    img.classList.add('evil-Goblin');
+    div.classList.add('evil-Goblinn');
+  // console.log("🚀 ~ file: app.js ~ line 49 ~ displayGodblins ~ goblin", data)
+    pName.textContent = data.name;
+    pHP.textContent = data.hp;
+    div.append(pName, img, pHP);
+    return div;
+}
