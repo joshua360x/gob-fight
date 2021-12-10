1680e68554148d3f82cf0e18ebca5ec6d11dcf25
joshua360x <jkw5499@psu.edu>
2021-12-10 15:42:26 -0500

    fixed if statements

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import { renderGoblin } from './utils.js';
 
 const goblinForm = document.querySelector('#new-goblin-form');
 const playerHP = document.querySelector('.playerHP');
-const playerChar = document.querySelector('.player-Char');
+// const playerChar = document.querySelector('.player-Char');
 const goblinChars = document.querySelector('.evil-Goblins');
 const goblinDefeated = document.querySelector('.numberDefeated');
 
@@ -29,12 +29,13 @@ let newGob = [
     }
 ];
 let defeated = 0;
+let playerHealth = 1;
 
-let defeatedGoblin = [
-    {
-        name: ''
-    }
-];
+// let defeatedGoblin = [
+//     {
+//         name: ''
+//     }
+// ];
 
 // set event listeners 
   // get user input
@@ -58,24 +59,46 @@ function displayGodblins() {
 
         let goblinList = renderGoblin(goblin);
 
+
         goblinList.addEventListener('click', () => {
             if ((Math.random() * 5) > 2) {
                 alert('you have hit the goblin');
                 goblin.hp = goblin.hp - (Math.ceil(Math.random() * 3));
+
+                if (goblin.hp <= 0) {
+                    goblin.hp = 0;
+                    defeated++;
+                    goblinDefeated.textContent = defeated;
+                    alert(`you have defeated ${goblin.name}`);
+                    displayGodblins(); 
+                }
+                
+                if ((Math.random() * 5) > 3) {
+                    alert('player has been hit');
+                    playerHealth--;
+                    playerHP.textContent = playerHealth;
+                    if (playerHealth === 0) {
+                        alert('Game Over');
+                    }
+                }
                 displayGodblins(); 
             } 
 
-            if (goblin.hp <= 0) {
-                goblin.hp = 0;
-                defeatedGoblin.push(goblin);
-                defeated++;
-                goblinDefeated.textContent = defeated;
-                alert(`you have defeated ${goblin.name}`);
+            // if (goblin.hp <= 0) {
+            //     goblin.hp = 0;
+            //     defeatedGoblin.push(goblin);
+            //     defeated++;
+            //     goblinDefeated.textContent = defeated;
+            //     alert(`you have defeated ${goblin.name}`);
+            //     if ((Math.random() * 5) > 3) {
+            //         alert('player has been hit');
+            //         playerHealth--;
+            //         playerHP.textContent = playerHealth;
+            //     } 
+            //         // console.log("🚀 ~ file: app.js ~ line 66 ~ goblinList.addEventListener ~ defeatedGoblin", defeatedGoblin)
           
-                    // console.log("🚀 ~ file: app.js ~ line 66 ~ goblinList.addEventListener ~ defeatedGoblin", defeatedGoblin)
-          
-                displayGodblins(); 
-            }
+            //     displayGodblins(); 
+            // }
 
 
 
